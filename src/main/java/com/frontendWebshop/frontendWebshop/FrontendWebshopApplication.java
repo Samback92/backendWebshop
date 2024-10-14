c3478dac9b02a643a247b38426f2a73f38fcdb6b
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-14 10:14:25 +0200

    first commit

---
+++ src/main/java/com/frontendWebshop/frontendWebshop/FrontendWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.frontendWebshop.frontendWebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frontendWebshop/frontendWebshop/FrontendWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontendWebshop.frontendWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
