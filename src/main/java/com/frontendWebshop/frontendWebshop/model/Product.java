43a1456f74708a57df1f1595784c1c601f5bde6f
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-17 12:53:11 +0200

    adds models for User and Product

---
+++ src/main/java/com/frontendWebshop/frontendWebshop/model/Product.java
@@ -0,0 +1,65 @@
+package com.frontendWebshop.frontendWebshop.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Products")
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String image;
+    private String description;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String name, String image, String description, double price) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ src/main/java/com/frontendWebshop/frontendWebshop/model/User.java
@@ -0,0 +1,76 @@
+package com.frontendWebshop.frontendWebshop.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+public class User {
+
+    @Id
+    private String id;
+    private String fName;
+    private String lName;
+    private String email;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String fName, String lName, String email, String username, String password) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
