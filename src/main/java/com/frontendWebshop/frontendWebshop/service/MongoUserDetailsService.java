f9c628e30269c53e998018ef3f2e546b78b3bea0
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-17 12:54:20 +0200

    Create MongoUserDetailsService.java

---
+++ src/main/java/com/frontendWebshop/frontendWebshop/service/MongoUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.frontendWebshop.frontendWebshop.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.frontendWebshop.frontendWebshop.model.User;
+import com.frontendWebshop.frontendWebshop.model.UserDto;
+
+import com.frontendWebshop.frontendWebshop.repository.UserRepository;
+
+@Service
+public class MongoUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public MongoUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserDto(user);
+    }
+}
