b397915da0b5b7812485813121e6c537f3b3e51d
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-30 10:50:54 +0100

    Create BackendWebshopApplicationTests.java

---
+++ src/test/java/com/backendWebshop/backendWebshop/BackendWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendWebshop.backendWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
