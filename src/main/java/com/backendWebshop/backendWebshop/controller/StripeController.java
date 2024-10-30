4fa98d910b4ccebe482aef09e3516496599313e3
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-30 10:51:25 +0100

    Update StripeController.java

--- src/main/java/com/backendWebshop/backendWebshop/controller/StripeController.java
+++ src/main/java/com/backendWebshop/backendWebshop/controller/StripeController.java
@@ -1,13 +1,10 @@
 package com.backendWebshop.backendWebshop.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.stripe.Stripe;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
-import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
